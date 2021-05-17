ba9e3dfb2d1b0917a0a5798e9a9a2e2356518a48
zohashikh <59775296+zohashikh@users.noreply.github.com>
2021-05-17 10:40:45 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './App.css';
+import Routes from "./Routes";
+import {Home} from './features/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+function App() {
+  return (
+    <Router>
+      <div>
+        <button><Link to="/">Home</Link></button>
+        <button><Link to="/about">About</Link></button>
+          <button><Link to="/register">Register</Link></button>
+        
+        <hr />
+        <Routes />
+      </div>
+       </Router>
+  );
+}
+
+export default App;
