30885011a4bf31d44a2eb0c92f0e0b7925c774fc
zohashikh <59775296+zohashikh@users.noreply.github.com>
2021-05-19 01:55:51 +0500

    Add files via upload

--- features/ShowUsers/ShowUsers.js
+++ features/ShowUsers/ShowUsers.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import './showusers.css'
-
+import RegistrationForm from '../Register/Register'
+import { useForm } from "react-hook-form";
 
 export default function Userdata() {
+ 
   const [email, setEmail] = useState([])
   const [password, setPassword] = useState([])
   const [confirmPassword,setConfirmPassword] = useState([])
@@ -21,27 +23,87 @@ export default function Userdata() {
             }
            ) 
            // document.getElementById("show").innerHTML = user[0].email
-            var email1 =[]
-          
-            for (let index = 0; index < user.length; index++) {
-            email1[index] = user[index].id
-           
-           
-           
-          } console.log(email1)
+        
          
-   const deleteuser = () => {
-     var id = document.getElementById("deletedata").value
-      
-console.log(id,user.findIndex(x => x.id == id),document.getElementById("deletedata").value)
-
-       user.splice(id,1)
-  console.log(user)
-
+   const deleteuser = (id) => {
+      user.splice(id-1,1)
+      console.log(user)
   
    
   } 
+  
+   const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => {
+    var id = user.length + 1
+   user.push({
+            id:id,
+             email:data.example,
+             password:data.exampleRequired
+            }
+           ) 
+   console.log(user)
 
+  }
+
+  
+   const Adduser = (
+     /*
+ <form onSubmit={handleSubmit(onSubmit)}>
+     
+      <p>Email</p>
+      <input  {...register("example", { required: true })} />
+      
+     
+      <p>Password</p>
+      <input {...register("exampleRequired", { required: true })} />
+     
+      {errors.exampleRequired && <span>This field is required</span>}
+      
+      <input type="submit" />
+    </form>    
+*/
+<p>hjxbc</p>
+);
+
+  const onSubmitupdate = data => {
+    if(data.example != null && data.exampleRequired != null){
+      for (let index = 0; index < user.length; index++) {
+           
+           if(user[index].id == data.id){
+             console.log(data.id) 
+               user[index].email = data.example
+               user[index].password = data.exampleRequired
+            console.log(user)
+           }else{
+    console.log('Id not found')
+  }
+           
+          }
+
+  
+  
+
+  }
+ 
+}
+   const update = (
+
+ <form onSubmit={handleSubmit(onSubmitupdate)}>
+      
+       <p>Enter Id to update certain user *number</p>
+      <input type="number" {...register("id", { required: true })} />
+      <p>Email</p>
+      <input  {...register("example", { required: true })} />
+      
+      <p>Password</p>
+      <input {...register("exampleRequired", { required: true })} />
+     
+      {errors.exampleRequired && <span>This field is required</span>}
+      
+      <input type="submit" />
+    </form>    
+
+);
  const users = user.map(users => (
  <div className="content">
   <table>
@@ -49,9 +111,8 @@ console.log(id,user.findIndex(x => x.id == id),document.getElementById("deleteda
        <td>{users.id}</td>
        <td>{users.email}</td>
        <td>{users.password}</td>
-       <td><button id="deletedata" value={user.findIndex(x => x.id == users.id)} onClick={deleteuser} >Delete</button></td>
-     <td><button  onClick={deleteuser} >Update</button></td>
-     </tr>
+       <td><button  onClick={()=>deleteuser(users.id)} >Delete</button></td>
+    </tr>
   </table>
     </div>
    
@@ -62,27 +123,31 @@ console.log(id,user.findIndex(x => x.id == id),document.getElementById("deleteda
 
  
   return (
+   
     <div>
     <div ></div>
-    <p id="s.how">
+    <p id="show">
       <table>
           <tr>
             <th>Id</th>
             <th>Email</th>
             <th>Password</th>
             <th>Delete</th>
-            <th>Update</th>
+           
            
           </tr>
    
       </table>
       {users}
-      <button>Add User</button></p>
+    </p>
     
 
 <p>No. of users: {user.length}</p>
     
+<h2>Add User</h2> {Adduser}
 
+<h2>Update</h2>
+{update}
     </div>
   )
             }
